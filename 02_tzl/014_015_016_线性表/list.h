8b3678403cabb154c9be88ecd125f95ae4114704
tom37 <764575907@qq.com>
2019-11-26 00:11:51 +0800

    解决编译bug

--- 02_tzl/014_015_016_线性表/list.h
+++ 02_tzl/014_015_016_线性表/list.h
@@ -1,5 +1,5 @@
-#ifndef SEQ_LIST_H
-#define SEQ_LIST_H
+#ifndef LIST_H
+#define LIST_H
 
 #include "object.h"
 
--- 02_tzl/014_015_016_线性表/main.cpp
+++ 02_tzl/014_015_016_线性表/main.cpp
@@ -6,7 +6,7 @@ using namespace DTLib;
 
 int main(int argc, char **argv)
 {
-	//SeqList<int> *s;
+	SeqList<int> *s;
 	
 	
 	return 0;
