362dd6ca27f09fa1a7c68c5d1c5d9e9d929c08d2
tom37 <764575907@qq.com>
2020-01-03 13:43:18 +0800

    add 冒泡排序和希尔排序

--- 02_tzl/3_data_structure/sort/main.cpp
+++ 02_tzl/3_data_structure/sort/main.cpp
@@ -8,7 +8,10 @@ int main(int argc, char **argv)
 {
     int array[] = {3,1,2,5,4};
     //Sort::Select(array, 5, 0);
-    Sort::Insert(array, 5, 1);
+    //Sort::Insert(array, 5, 1);
+    //Sort::Bubble(array, 5, 0);
+    //Sort::Shell_from_insert(array, 5, 0);
+    Sort::Shell_from_bubble(array, 5, 0);
 
     for (int i = 0; i < 5; i++) {
         cout << array[i] << endl;
--- 02_tzl/3_data_structure/sort/sort.h
+++ 02_tzl/3_data_structure/sort/sort.h
@@ -60,6 +60,72 @@ public:
         }
     }
 
+    // 冒泡排序，该排序是稳定的
+    template <typename T>
+    static void Bubble(T array[], int len, bool min2max = true) // O(n2)
+    {
+        bool exchange = true;
+
+        for (int i = 0; (i < len) && exchange; i++) {
+            exchange = false;
+            for (int j = len - 1; j > i; j--) {
+                if (min2max ? (array[j] < array [j - 1]) : (array[j] > array [j - 1])) {
+                    Swap(array[j], array[j - 1]);
+                    exchange = true;
+                }
+            }
+        }
+    }
+
+    /* 希尔排序 - 插入排序
+     * 基本思想：将待排序列划分为若干组，在每一组内进行插入排序，以使整个序列基本有序，然后再对整个序列进行插入排序
+     */
+    template <typename T>
+    static void Shell_from_insert(T array[], int len, bool min2max = true) // O(n3/2)
+    {
+        int d = len;
+        do {
+            d = d / 3 + 1; // 实践证明这样减小d比较好，也可以d--, 但效率不会好
+            for (int i = d; i < len; i += d) {
+                int k = i;
+                T e = array[i];
+
+                for (int j = i - d; (j >= 0) && (min2max ? (array[j] > e) : (array[j] < e)); j -= d) {
+                    array[j + d] = array[j];
+                    k = j;
+                }
+
+                if (k != i) {
+                    array[k] = e;
+                }
+            }
+        } while(d > 1);
+    }
+#if 1
+    /* 希尔排序 - 冒泡排序
+     * 基本思想：将待排序列划分为若干组，在每一组内进行冒泡排序，以使整个序列基本有序，然后再对整个序列进行冒泡排序
+     */
+    template <typename T>
+    static void Shell_from_bubble(T array[], int len, bool min2max = true) // O(n3/2)
+    {
+        int d = len;
+        do {
+            d = d / 3 + 1; // 实践证明这样减小d比较好，也可以d--, 但效率不会好
+            bool exchange = true;
+            for (int i = d; (i < len) && exchange; i += d) {
+                exchange = false;
+                for (int j = i; j < len - 1; j += d) {
+                    if (min2max ? (array[j] < array [j + d]) : (array[j] > array [j + d])) {
+                        Swap(array[j], array[j + d]);
+                        exchange = true;
+                    }
+                }
+            }
+        } while(d > 1);
+    }
+#endif
+
+
 };
 
 
